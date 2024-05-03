60d00d1ec6eff8340538299390fb5e330d04adb8
melkhaloufi <91948711+melkhaloufi@users.noreply.github.com>
2024-05-03 15:11:16 +0200

    Add files via upload

---
+++ YoutubeMiner/src/test/java/aiss/YoutubeMiner/YoutubeMinerApplicationTests.java
@@ -0,0 +1,13 @@
+package aiss.YoutubeMiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeMinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
